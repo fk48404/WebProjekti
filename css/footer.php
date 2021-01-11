19c5903c228bedea9f64b5a446acc13f330719d7
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-01-11 19:13:12 +0100

    Create footer.php

---
+++ css/footer.php
@@ -0,0 +1,11 @@
+
+	<div id="footer">
+		<h2>Littlest Pet Shop</h2>
+		<p>535 Connecticut Ave, Suite 305</br>
+			Norwalk, CT, 06854</br>
+			(203) 750-5200</p>
+		<br></br>
+		<div id="lastFooter">
+		<p>&copy; 2020 Littlest Pet Shop </p>
+		</div>
+	</div>
