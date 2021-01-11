993f282963c93337e796932c9cd7f9e088a7b742
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-01-11 17:15:24 +0100

    Create Html -> PHP About.php

---
+++ Html -> PHP About.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us</title>
+        <link href="css/about.css" rel="stylesheet"/>  
+    </head>
+    <body>
+    <!--HEADER-->
+           <?php include 'include/header.php'; ?>
+        <!-- Rreth nesh -->
+        <div class="div1"> 
+            <div class="inner-div1">
+                <h1>Disa gjera rreth nesh:</h1>
+            </br>
+                <article>
+                    <img src="images/lpslogo2.png" id="foto-div1">
+                </article>
+                <aside>
+                    <p id="teksti-div1">
+                        Qe nga viti 2010, ne kemi ngritur nivelin per te ashtuquajturen objekt te kujdesit per kafshet shtepiake. </br>
+                        Te gjitha qendrat e Littlest Pet Shop ofrojne suita individuale te pastra dhe te kontrolluara nga klima, </br> 
+                        me ushqim premium, shtrat dhe nje menu te gjere te aktiviteteve dhe sherbimeve te personalizuara ne dispozicion.
+                        </br>
+                        </br>
+                        E gjithe kjo pune eshte e organizuar nga nje staf jo i gjere,</br>
+                         por kohe pas kohe kemi dhe vullnetare qe deshirojne te jene pjese e jona me mundesi te tyre.
+                        </br>
+                         </br>
+                        Kur nuk mund te jeni atje qe te kujdeseni ju, ne ofrojme nje vend per t'u kujdesur qe kafsha juaj te qendroje, </br>
+                        te luaje dhe te jete ne shoqeri te kafsheve te tjera ne menyre qe te mos ndihet i vetmuar.
+                        </br>
+                        </br>
+                        Ne perveq se mund te kujdesemi per kafshen tuaj perkohesisht, kemi edhe kafshet tona te cilat vizitohen nga vizitues te shpeshte,
+                        Vizita qe ju apo femijet tuaj mund te beni eshte pa pagese, por qe per kete duhet te na kontaktoni paraprakisht.
+                    </p>  
+                </aside> 
+            </div>
+        </div>
+
+        <div class="div2">
+            <div id="sect1">
+                <h2>Stafi:</h2>
+                <p>Nereida Zhitopotoku</p>
+                <p>Fortesa Krasniqi</p>
+                <p>Blerona Zariqi</p>
+                <p>Erleta Bahtiri</p>
+                <p>Mirjeta Shala</p>
+                <p>Igballe Terziqi</p>
+            </div>
+            
+            <div id="sect2">
+                <h2>Orari i Punes:</h2>
+                <p>E Hene - E Premte</p>
+                <p>09:00 - 19:00</p>
+                <p>Vikendin e kemi pushim!</p>
+            </div>
+        </div>
+       
+        <div class="div3">
+            <div class="video-div3">
+                <video width="500" controls>
+                    <source src="images/baby_dog6.mp4" type="video/mp4">
+                </video>
+                <video width="500" controls>
+                    <source src="images/baby-cats-playing.mp4" type="video/mp4">
+                </video>
+            </div>   
+
+            <div class="foto1-div3">
+                <img src="images/cats1.png">
+                <img src="images/dogi17.png">
+                <img src="images/dogi4.png">
+            </div>
+
+            <div class="foto2-div3">
+                <img src="images/cutebunny.png">
+                <img src="images/dogi3.png">
+                <img src="images/giphy.gif">
+            </div>
+
+            <div class="foto3-div3">
+                <img src="images/lepur3.png">
+                <img src="images/mace1.png">
+                <img src="images/dogi7.png">
+            </div>
+
+            <div class="foto4-div3">
+                <img src="images/lepur2.png">
+                <img src="images/dogi6.png">
+                <img src="images/dogi8.png">
+            </div>
+        </div>
+
+<footer>
+		<?php include 'include/footer.php'; ?>
+</footer>
+        
+    </body>
+</html>
