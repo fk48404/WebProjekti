8b159c2353092b1ddc0d4eaced26bdbd1a3e1cbe
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-01-13 11:32:03 +0100

    Create educateMe.php

---
+++ educateMe.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Educate Me</title>
+	<link rel="stylesheet" type="text/css" href="css/educateMe.css">
+</head>
+<body>
+<!--HEADER-->
+           <?php include 'include/header.php'; ?>
+<br></br>
+<!-- Content Starts -->
+
+	<h1 id="educateMe">Educate me</h1><br>
+	<p id="eM">Me poshte gjeni fakte rreth qenve,maceve dhe lepujve që do t'ju bëjnë të <b>dashuroheni</b> përsëri...☺ </p>
+
+	<section class="section2" id="dog-cat">
+		<article>
+		<p>Qeni/macja mund të lexojë gjendjet tuaja shpirtërore.</br>
+			 Nëse jeni të trishtuar ose jeni nën stres, mund të vëreni gjithashtu një ndryshim në sjelljen 
+			 e qenit ose maces suaj.</p>
+		</article>
+			<img src="images/first.png" alt="">
+		</aside>
+	</section>
+
+	<section class="section1" id="cat1">
+		<article>
+			<img src="images/ct.png" alt="">
+		</article>
+		<aside>
+			<p>-Macet nuk mjaullojnë tek macet e tjera. 
+				Ata e rezervojnë këtë tingull për të tërhequr
+				 vëmendjen nga njerëzit.<br></br>
+			-Macet kanë një shikim të fuqishëm të natës, duke i bere ata të shohin në nivele
+				 drite gjashtë herë më të ulëta se ato që i duhen një njeriu për të parë.</p>
+		</aside>
+	</section>
+
+	<section class="section2" id="bunny1">
+		<article>
+			<p>Një nga lepujt më të njohur në botë është personazhi i karikaturave të Warner Bros, Bugs Bunny -
+				 i cili shpesh është parë të konsumon mjafte karotë!</br> Në fakt, karotat nuk janë një pjesë e natyrshme e 
+				ushqimit të një lepuri dhe mund t'u shkaktoje probleme ne stomak nëse hanë shumë.</p>
+		</article>
+		<aside>
+			<img src="images/bk.png" alt="">
+		</aside>
+	</section>
+
+	<section class="section1" id="bulldog">  
+		<article >
+			<img src="images/bulldog.png" alt="">
+		</article>
+		<aside>
+			<p>Qentë me fytyrë të “dërrmuar” kanë më shumë probleme shëndetësore. 
+				Struktura e fytyrave të pugs, boksierëve dhe bulldogëve i bën ata më të prirur ndaj problemeve të frymëmarrjes,
+				 problemeve të dhëmbëve dhe çështjeve të tjera shëndetësore.</p>
+		</aside>
+	</section>
+
+	<section class="section2" id="bunny2">
+		<article>
+			<p>-Dhëmbët e një lepuri nuk ndalen kurrë së rrituri!<br></br>
+			   -Lepurushkat nuk mund të vjellin, 
+				prandaj është jashtëzakonisht e rëndësishme t'i ushqeni ata vetëm me ushqime të shëndetshme, 
+				të freskëta dhe të përshtatshme.
+				</p>>
+		</article>
+		<aside>
+			<img src="images/bTeeth.png" alt="">
+		</aside>
+	</section>
+	<section class="section1" id="blueEyes">
+		<article>
+			<img src="images/blueCat.png" alt="">
+		</article>
+		<aside>
+			<p>-Të gjithë kotelet kanë lindur me sy blu. Ata fillojnë të ndryshojnë ngjyrën rreth dy javë pasi u hapen sytë.<br></br>
+			    -Mesatarisht macet jetojnë për rreth 12 deri në 15 vjet.</p>
+		</aside>
+	</section>
+	<br></br>
+<!-- Footer Starts -->
+<footer>
+		<?php include 'include/footer.php'; ?>
+</footer>
+	
+	</body>
+	</html>
